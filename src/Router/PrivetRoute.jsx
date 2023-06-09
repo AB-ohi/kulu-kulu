1a0d8c86ed89efcdeb460619cac2f44b821a3897
AB-ohi <askohi63907@gmail.com>
2023-06-09 17:53:48 +0600

    vfndj

--- src/Router/PrivetRoute.jsx
+++ src/Router/PrivetRoute.jsx
@@ -2,7 +2,7 @@ import { useContext } from "react";
 import { AuthContext } from "../Provider/AuthProvider";
 import { useLocation } from "react-router-dom";
 
-const PrivetRoute = () => {
+const PrivetRoute = ({children}) => {
     const { user,loading } = useContext(AuthContext);
     const location = useLocation()
     if(loading){
